8091499658c4230a0de4cfd5fdd46d313d2870fe
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-07-19 16:46:05 +0200

    Initial commit

---
+++ src/main/java/com/paymybuddy/paymybuddyapp/PaymybuddyappApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paymybuddyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddyappApplication.class, args);
+	}
+
+}
