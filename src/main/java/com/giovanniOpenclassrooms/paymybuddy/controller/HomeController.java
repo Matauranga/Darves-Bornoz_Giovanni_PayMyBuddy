035f379fae03f5e537596c790ab4cccde07d4546
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-08-17 16:57:52 +0200

    Added the home page with its controller and test

---
+++ src/main/java/com/giovanniOpenclassrooms/paymybuddy/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.giovanniOpenclassrooms.paymybuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    /**
+     * Handler method to handle profile request
+     *
+     * @return the home page
+     */
+    @GetMapping("/home")
+    public String profile() {
+
+        return "home";
+    }
+
+}
---
+++ src/test/java/com/giovanniOpenclassrooms/paymybuddy/controller/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.giovanniOpenclassrooms.paymybuddy.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("Try to perform method get on /home")
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk());
+
+    }
+}
