e9d1d7ec51749a67b0d809419856e50b8c0b712d
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-08-17 17:25:14 +0200

    Added UserDetailsService test

---
+++ src/test/java/com/giovanniOpenclassrooms/paymybuddy/business/CustomUserDetailsServiceTest.java
@@ -0,0 +1,60 @@
+package com.giovanniOpenclassrooms.paymybuddy.business;
+
+import com.giovanniOpenclassrooms.paymybuddy.model.Person;
+import com.giovanniOpenclassrooms.paymybuddy.repository.PersonRepository;
+import com.giovanniOpenclassrooms.paymybuddy.utils.PersonFaker;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class CustomUserDetailsServiceTest {
+    @Mock
+    private PersonRepository userRepository;
+
+    @InjectMocks
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Test
+    void loadUserByUsername() {
+        //Given
+        final Person person = PersonFaker.generate();
+        final String personEmail = person.getEmail();
+
+        //When
+        when(userRepository.findByEmail(personEmail)).thenReturn(person);
+        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(personEmail);
+
+        //Then
+        assertNotNull(userDetails);
+        assertEquals(person.getEmail(), ReflectionTestUtils.getField(userDetails, "username"));
+    }
+
+    @Test
+    void loadUserByUsernameFailed() {
+        //Given
+        final Person person = PersonFaker.generate();
+        final String personEmail = person.getEmail();
+
+        //When
+        when(userRepository.findByEmail(any())).thenReturn(null);
+        assertThrows(UsernameNotFoundException.class, () -> {
+            customUserDetailsService.loadUserByUsername(personEmail);
+        });
+
+        //Then
+
+    }
+
+    @Test
+    void getAuthorities() { //TODO
+    }
+}
